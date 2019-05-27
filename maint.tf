b9e2d585f3f967c4aa52a01130a360bc1af6375f
nta <nico.taroy@gmail.com>
2019-05-27 14:58:41 +0200

    bla

--- maint.tf
+++ maint.tf
@@ -1,4 +1,4 @@
 resource "local_file" "myfile" {
-  content  = "hello !"
-  filename = "/tmp/monfic.txt"
+  content  = "hello Nico !"
+  filename = " /media/nta/124GB/AWS/test_terraform/monfic.txt"
 }
