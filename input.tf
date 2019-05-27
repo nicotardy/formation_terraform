2788f20c0d50d7334f8da9d20425b74661576dda
nta <nico.taroy@gmail.com>
2019-05-27 15:06:17 +0200

    input file

---
+++ input.tf
@@ -0,0 +1,4 @@
+variable "input_filename" {
+  default = "/media/nta/124GB/AWS/test_terraform/monfic.txt"
+  type    = "string"
+}
--- maint.tf
+++ maint.tf
@@ -1,4 +1,4 @@
 resource "local_file" "myfile" {
   content  = "hello Nico !"
-  filename = " /media/nta/124GB/AWS/test_terraform/monfic.txt"
+  filename = "/media/nta/124GB/AWS/test_terraform/monfic.txt"
 }
