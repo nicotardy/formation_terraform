883f748d156152335ec479f144176caef6afacaf
nta <nico.taroy@gmail.com>
2019-05-28 11:02:03 +0200

    ajout du verrou d'utilsation du tfstate dans AWS avec dynamoDB

--- bootstrap/init.tf
+++ bootstrap/init.tf
@@ -6,3 +6,16 @@ resource "aws_s3_bucket" "terraform_backend" {
   bucket = "lab-nta-tfstate-bucket"
   acl    = "private"
 }
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = "foo"
+
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
